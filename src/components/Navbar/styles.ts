ebc0b0187e59afff9524688119af7097a0f2ea27
Flavio Ever <flavio.ever@icloud.com>
2021-11-17 19:28:39 -0300

    Teste prod

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -9,11 +9,11 @@ export const List = styled.ul<List>(({ visible }) => [
   tw`flex flex-col sm:flex-row justify-center items-center`,
   tw`min-h-screen sm:min-h-0`,
   tw`fixed sm:relative`,
-  tw`top-0 right-0 left-0 z-10`,
+  tw`top-0 right-0 left-0`,
   tw`space-y-6 sm:flex sm:space-y-0 sm:space-x-4`,
   tw`transition-all duration-500 sm:transition-none`,
 
-  visible && tw`-top-full`
+  visible && tw`-top-full z-0`
 ])
 
 export const Item = styled.li(() => [
