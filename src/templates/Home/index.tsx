bf723f538e4ebe020e3000eda1b6db04308cd280
Flavio Ever <flavio.ever@icloud.com>
2021-11-22 17:38:20 -0300

    Apresentacao, transicao cores card slim

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -30,9 +30,8 @@ export default function HomeTemplate() {
               <div tw="flex flex-col text-center sm:text-left space-y-4 md:mr-6">
                 <h1 tw="text-purple-50 text-5xl">{`<OlaMundo/>`}</h1>
                 <p tw="px-4 sm:px-0">
-                  <br /> Bem vindo(a) ao meu portfólio/Blog
-                  <br /> Nodezeiro 🚀 na semana e Player casual 🎮 nas horas
-                  vagas :P
+                  Bem vindo(a) ao meu portfólio/Blog. Nodezeiro 🚀 na semana e
+                  Player casual 🎮 nas horas vagas :P
                 </p>
               </div>
               <div tw="mb-5">
